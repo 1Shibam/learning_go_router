fdbe3dab749abc2e460422d56a8d5a9ead25ac1b
1Shibam <hehe.shibam@gmail.com>
2025-02-03 23:42:42 +0530

    screen 1 navigation!!

--- lib/screens/screen1.dart
+++ lib/screens/screen1.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
+import 'package:learning_go_router/preferences/on_boarding_preference.dart';
 
 class Screen1 extends StatelessWidget {
   const Screen1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'screen 1',
+            const Text(
+              'you should see the onboarding screen!!',
               style: TextStyle(fontSize: 60),
             ),
-           
+            ElevatedButton(
+                onPressed: () {
+                  setOnBoardingTrue();
+                  context.go('/screen2');
+                },
+                child: const Text('i have seen it now!!'))
           ],
         ),
       ),
