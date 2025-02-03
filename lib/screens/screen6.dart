6f388dddec5d11c2086eb7608c1120451404539f
1Shibam <hehe.shibam@gmail.com>
2025-02-03 22:12:01 +0530

    adds multiple screens for routing!!

---
+++ lib/screens/screen6.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Screen6 extends StatelessWidget {
+  const Screen6({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Screen 1',
+          style: TextStyle(fontSize: 60),
+        ),
+      ),
+    );
+  }
+}
