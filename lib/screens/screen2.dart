c3efc41811d84ac84af3b6751222d69549e0d7cf
1Shibam <hehe.shibam@gmail.com>
2025-02-03 23:42:53 +0530

    screen 2 navigation!!

--- lib/screens/screen2.dart
+++ lib/screens/screen2.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:learning_go_router/preferences/log_in_preference.dart';
+import 'package:learning_go_router/preferences/on_boarding_preference.dart';
 
 class Screen2 extends StatelessWidget {
   const Screen2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          'Screen 2',
-          style: TextStyle(fontSize: 60),
+        child: Column(
+          children: [
+            const Text(
+              'Your are not logged in !!',
+              style: TextStyle(fontSize: 60),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  loggedIn();
+                  context.go('/screen3');
+                },
+                child: const Text('let me log in!!')),
+            ElevatedButton(
+                onPressed: () {
+                  setOnBoardingFalse();
+                  
+                  context.go('/screen1');
+                },
+                child: const Text('see on Boarding Again!!')),
+          ],
         ),
       ),
     );
