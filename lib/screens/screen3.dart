104a3ec87d92948146862e32ce29705dc5054662
1Shibam <hehe.shibam@gmail.com>
2025-02-03 23:43:04 +0530

    screen 3 navigation!!

--- lib/screens/screen3.dart
+++ lib/screens/screen3.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:learning_go_router/preferences/log_in_preference.dart';
 
 class Screen3 extends StatelessWidget {
   const Screen3({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          'Screen 3',
-          style: TextStyle(fontSize: 60),
+        child: Column(
+          children: [
+            const Text(
+              'you are logged in!!',
+              style: TextStyle(fontSize: 60),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  loggedOut();
+                  context.go('/screen2');
+                },
+                child: const Text('log out now!'))
+          ],
         ),
       ),
     );
